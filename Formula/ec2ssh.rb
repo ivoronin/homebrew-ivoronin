89ee0e7eea6fa2c73777e331a6960167ae8aa88e
Ilya Voronin <ivoronin@gmail.com>
2025-12-09 12:34:10 +0200

    ec2ssh 2025.12.09
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ec2ssh.rb
+++ Formula/ec2ssh.rb
@@ -1,8 +1,8 @@
 class Ec2ssh < Formula
   desc "Simplify SSH connections to AWS EC2 instances"
   homepage "https://github.com/ivoronin/ec2ssh"
-  url "https://github.com/ivoronin/ec2ssh/archive/refs/tags/2025.12.05.tar.gz"
-  sha256 "dd7a52bbf13ff32f99ea216572ed895f25e436cfa55bef1bb4e9519365b7f9aa"
+  url "https://github.com/ivoronin/ec2ssh/archive/refs/tags/2025.12.09.tar.gz"
+  sha256 "d87eedf9dc792a4dcf110872ed10b3cd8a4fea90f1f1573414c4260c0bd9bc20"
   license "GPL-3.0-only"
   head "https://github.com/ivoronin/ec2ssh.git", branch: "main"
 
