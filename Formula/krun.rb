e99a5269ca967cbaf279b1442068f1d47344bdec
Ilya Voronin <ivoronin@gmail.com>
2026-01-09 14:12:32 +0200

    krun 2026.01.09
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/krun.rb
+++ Formula/krun.rb
@@ -1,8 +1,8 @@
 class Krun < Formula
   desc "Run one-off commands in Kubernetes pods interactively"
   homepage "https://github.com/ivoronin/krun"
-  url "https://github.com/ivoronin/krun/archive/refs/tags/2024.11.11.tar.gz"
-  sha256 "c939085c8dda2c22da552d613b9c37c329f118b25e1a32e3db01be8383a5fafe"
+  url "https://github.com/ivoronin/krun/archive/refs/tags/v2026.01.09.tar.gz"
+  sha256 "eb6690de70a27965255e0e081fa0178cef51e05a4a5fb8201de301551d93907d"
   license :cannot_represent
   head "https://github.com/ivoronin/krun.git", branch: "main"
 
