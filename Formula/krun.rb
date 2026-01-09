e295182966a968a72830db43e4d44cbc1ae40202
Ilya Voronin <ivoronin@users.noreply.github.com>
2026-01-09 14:13:11 +0200

    Update krun formula for cmd/ structure and version injection

--- Formula/krun.rb
+++ Formula/krun.rb
@@ -3,16 +3,20 @@ class Krun < Formula
   homepage "https://github.com/ivoronin/krun"
   url "https://github.com/ivoronin/krun/archive/refs/tags/v2026.01.09.tar.gz"
   sha256 "eb6690de70a27965255e0e081fa0178cef51e05a4a5fb8201de301551d93907d"
-  license :cannot_represent
+  license "GPL-3.0"
   head "https://github.com/ivoronin/krun.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/#{name}"
   end
 
   test do
-    assert_match "krun", shell_output("#{bin}/krun --help 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/#{name} --version")
   end
 end
