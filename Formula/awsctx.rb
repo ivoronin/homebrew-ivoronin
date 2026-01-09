e5e2369ec2d1a04cdb16ff1768687d082333a107
Ilya Voronin <ivoronin@gmail.com>
2026-01-09 10:27:01 +0000

    awsctx 2025.01.09

--- Formula/awsctx.rb
+++ Formula/awsctx.rb
@@ -1,8 +1,8 @@
 class Awsctx < Formula
   desc "Tool to manage AWS SDK configuration profiles inspired by kubectx"
   homepage "https://github.com/ivoronin/awsctx"
-  url "https://github.com/ivoronin/awsctx/archive/refs/tags/v2024.08.10.tar.gz"
-  sha256 "78708555b58031f9e488eb380e5d95beb67a8eab20ee7a3391067d01b31df62f"
+  url "https://github.com/ivoronin/awsctx/archive/refs/tags/v2025.01.09.tar.gz"
+  sha256 "7ed033eb939184856c738247cd87dd911558cc46c8ce1b1d5c02ce88bee34f55"
   license "GPL-3.0"
   head "https://github.com/ivoronin/awsctx.git", branch: "main"
 
