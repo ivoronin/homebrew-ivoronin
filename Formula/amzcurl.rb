0d6ec7d7ed089b7c1eaa729d76b93b471ad2a837
Ilya Voronin <ivoronin@users.noreply.github.com>
2026-01-09 13:58:35 +0200

    Update amzcurl formula for cmd/ structure and version injection

--- Formula/amzcurl.rb
+++ Formula/amzcurl.rb
@@ -3,16 +3,20 @@ class Amzcurl < Formula
   homepage "https://github.com/ivoronin/amzcurl"
   url "https://github.com/ivoronin/amzcurl/archive/refs/tags/v2026.01.09.tar.gz"
   sha256 "e184fd7f14382c928868ea64aac9348ec86c2a38fc0c066d7b55a07dddbf0d91"
-  license :cannot_represent
+  license "GPL-3.0"
   head "https://github.com/ivoronin/amzcurl.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/#{name}"
   end
 
   test do
-    assert_match "amzcurl", shell_output("#{bin}/amzcurl --help 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/#{name} --version")
   end
 end
