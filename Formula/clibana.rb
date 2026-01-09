7612e6fa5b002f3729e8d8bd8a2c1b1486fec03b
Ilya Voronin <ivoronin@users.noreply.github.com>
2026-01-09 14:22:34 +0200

    Update clibana formula for cmd/ structure

--- Formula/clibana.rb
+++ Formula/clibana.rb
@@ -3,7 +3,7 @@ class Clibana < Formula
   homepage "https://github.com/ivoronin/clibana"
   url "https://github.com/ivoronin/clibana/archive/refs/tags/v2026.01.09.tar.gz"
   sha256 "4caaddb6951af54e7970a096cb7e365adf49998146c9e58cdba14f9f0e139dc1"
-  license :cannot_represent
+  license "GPL-3.0"
   head "https://github.com/ivoronin/clibana.git", branch: "main"
 
   depends_on "go" => :build
@@ -15,10 +15,10 @@ class Clibana < Formula
       -X main.commit=v#{version}
       -X main.date=#{time.iso8601}
     ]
-    system "go", "build", *std_go_args(ldflags:)
+    system "go", "build", *std_go_args(ldflags:), "./cmd/#{name}"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/clibana --version")
+    assert_match version.to_s, shell_output("#{bin}/#{name} --version")
   end
 end
