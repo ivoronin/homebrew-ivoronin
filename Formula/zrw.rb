4abfa691a5aa675a32c171d513f7a947d8c61919
Ilya Voronin <ivoronin@gmail.com>
2026-01-24 00:33:43 +0200

    zrw 2026.01.23
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/zrw.rb
+++ Formula/zrw.rb
@@ -1,8 +1,8 @@
 class Zrw < Formula
   desc "Run commands in Zellij panes and wait for exit code"
   homepage "https://github.com/ivoronin/zrw"
-  url "https://github.com/ivoronin/zrw/archive/refs/tags/v2026.01.21.tar.gz"
-  sha256 "acd94e709a3916e41cfa7f037389fbf9bb29c2e399c19b7c742e3e18ddc087a6"
+  url "https://github.com/ivoronin/zrw/archive/refs/tags/v2026.01.23.tar.gz"
+  sha256 "a3f771fce5e1a0fba1dba0a65df3eeea3414b91989cf652747688dbab4d31ed6"
   license "GPL-3.0"
   head "https://github.com/ivoronin/zrw.git", branch: "main"
 
