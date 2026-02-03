6445033a2c6b64997f73b22c90046cf68aafd219
Ilya Voronin <ivoronin@gmail.com>
2026-02-03 10:45:58 +0200

    wch 2026.02.03
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/wch.rb
+++ Formula/wch.rb
@@ -1,8 +1,8 @@
 class Wch < Formula
   desc "Yet another watch(1) clone"
   homepage "https://github.com/ivoronin/wch"
-  url "https://github.com/ivoronin/wch/archive/refs/tags/v2026.02.01.tar.gz"
-  sha256 "9a45bb91def46acd41e4f4bd67709b7737afc9ec2f0805c0f52cecd3e1fdc377"
+  url "https://github.com/ivoronin/wch/archive/refs/tags/v2026.02.03.tar.gz"
+  sha256 "351187abc4c3e8449c7a5553307d52be56e2478e46650e72ccfebe8a004d82a3"
   license "GPL-3.0"
   head "https://github.com/ivoronin/wch.git", branch: "main"
 
