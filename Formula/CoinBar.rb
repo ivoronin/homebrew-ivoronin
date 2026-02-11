6e318b13f058cb75c29b6b4d6c6e0a3c1102110f
Ilya Voronin <ivoronin@gmail.com>
2026-02-11 12:45:21 +0200

    CoinBar 2026.02.11

--- Formula/CoinBar.rb
+++ Formula/CoinBar.rb
@@ -1,8 +1,8 @@
 class Coinbar < Formula
   desc "Track cryptocurrency prices from the macOS menu bar"
   homepage "https://github.com/ivoronin/CoinBar"
-  url "https://github.com/ivoronin/CoinBar/archive/refs/tags/v2026.02.11.tar.gz"
-  sha256 "e957aa23e730ddcd6d63c0a56e27acb5fc52588ff1fd1bceda3a35e03e2c7354"
+  url "https://github.com/ivoronin/CoinBar/archive/refs/tags/2026.02.11.tar.gz"
+  sha256 "beab9c440341ffa0804e45c61cee9730b567d118ad5e4d9ee6cad15a939eca97"
   license "GPL-3.0-only"
   head "https://github.com/ivoronin/CoinBar.git", branch: "main"
 
