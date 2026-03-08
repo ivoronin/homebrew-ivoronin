3e64c3dead349e69b012fa9e359edcc3e0060bef
Ilya Voronin <ivoronin@gmail.com>
2026-03-08 14:12:08 +0200

    pll 2026.03.08
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/pll.rb
+++ Formula/pll.rb
@@ -1,8 +1,8 @@
 class Pll < Formula
   desc "Run shell commands in parallel with output buffering and resumable checkpoints"
   homepage "https://github.com/ivoronin/pll"
-  url "https://github.com/ivoronin/pll/archive/refs/tags/v2026.02.17.tar.gz"
-  sha256 "0cbd314a409bd991845976afa952c58c5b25ceb4bb0f7f48d763026ae68ac90d"
+  url "https://github.com/ivoronin/pll/archive/refs/tags/v2026.03.08.tar.gz"
+  sha256 "c0e3e5b56c2cb992b4855b03a421756706aead67a36b6361c2a825c4792a5b69"
   license "GPL-3.0"
   head "https://github.com/ivoronin/pll.git", branch: "master"
 
